7e6c0986ccca5cfe0dd5f796ff2779d2d9598b6c
enzo034 <enzo03453@gmail.com>
2024-10-12 14:37:23 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,77 @@
+import express from 'express';
+import crypto from 'crypto';
+import 'dotenv/config'
+
+const app = express();
+
+app.use(express.json({
+    verify: (req, res, buf) => {
+        req.rawBody = buf.toString();
+    }
+}))
+
+const MERCHANT_ID = process.env.MERCHANT_ID;
+const API_KEY = process.env.API_KEY;
+
+app.post('/checkout', async (req, res) => {
+
+    const payload = {
+        amount: '10',
+        currency: 'USD',
+        order_id: crypto.randomBytes(12).toString('hex'),
+        url_success: '', //ruta del proyecto en caso de que el pago se haya hecho con éxito,
+        url_return: '', //volver al sitio web inicial
+        callback_url: '' // para la utilización de webhooks
+    }
+
+    const sign = crypto
+        .createHash('md5')
+        .update(Buffer.from(JSON.stringify(payload)).toString('base64') + API_KEY)
+        .digest("hex");
+
+    const response = await fetch('https://api.cryptomus.com/v1/payment', {
+        method: 'POST',
+        body: JSON.stringify(payload),
+        headers: {
+            'Content-Type': 'application/json',
+            merchant: MERCHANT_ID,
+            sign: sign
+        }
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+
+    return res.json(data);
+
+});
+
+app.post('webhook', async (req, res) => {
+
+    const { sign } = req.body;
+
+    if (!sign) {
+        return res.status(400).json({ message: 'Invalid request' });
+    }
+
+    const data = JSON.parse(req.rawBody);
+    delete data.sign;
+
+    const calculatedSign = crypto.createHash('md5')
+        .update(Buffer.from(JSON.stringify(data)).toString('base64') + API_KEY)
+        .digest('hex');
+
+    if (calculatedSign !== sign) {
+        return res.status(400).json({ message: 'Invalid sign' });
+    }
+
+    console.log(req.body);
+
+    return res.sendStatus(200);
+
+});
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
